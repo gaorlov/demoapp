177656d398419ebb5871bc3001a6612f9559bc35
Jeff Ching <ching.jeff@gmail.com>
2016-06-21 15:22:44 -0700

    run tests in the container, separate the pipeline

--- ci/run.sh
+++ ci/run.sh
@@ -5,10 +5,10 @@ set -e -x
 # bundle install --without production deployment
 # bin/rake test
 
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
-apt-get install -y nodejs
-
-pushd master
-  bundle install
-  bin/rake
-popd
+# curl -sL https://deb.nodesource.com/setup_4.x | bash -
+# apt-get install -y nodejs
+#
+# pushd master
+bundle install --local --deployment --without deployment
+bin/rake
+# popd
